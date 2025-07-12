7268ce2e28dbdaa4d64258b2b9563a1cc0823f50
Baachi13 <jeremiasavaro7@gmail.com>
2025-07-12 18:26:37 -0300

    add fields quantity, location, status, etc to Glassplate

---
+++ Aberturas/db/migrate/20250712212123_add_fields_to_glassplates.rb
@@ -0,0 +1,10 @@
+class AddFieldsToGlassplates < ActiveRecord::Migration[8.0]
+  def change
+    add_column :glassplates, :thickness, :string
+    add_column :glassplates, :standard_measures, :string
+    add_column :glassplates, :quantity, :integer
+    add_column :glassplates, :location, :string
+    add_column :glassplates, :status, :string
+    add_column :glassplates, :is_scrap, :boolean
+  end
+end
