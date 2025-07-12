e35ed0f2451563946021f69eae5b5f627823a859
Baachi13 <jeremiasavaro7@gmail.com>
2025-07-12 18:35:46 -0300

    update schema after glass_type migration

--- Aberturas/db/schema.rb
+++ Aberturas/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_212123) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_213246) do
   create_table "glassplates", force: :cascade do |t|
     t.float "width"
     t.float "height"
     t.string "color"
-    t.string "type"
+    t.string "glass_type"
     t.boolean "deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
