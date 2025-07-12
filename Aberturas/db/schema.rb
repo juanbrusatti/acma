fff9ca53bc34c70e33caf8f25b0c6051205145fe
Baachi13 <jeremiasavaro7@gmail.com>
2025-07-12 18:28:27 -0300

    add sample data to seeds and update schema

--- Aberturas/db/schema.rb
+++ Aberturas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_205346) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_212123) do
   create_table "glassplates", force: :cascade do |t|
     t.float "width"
     t.float "height"
@@ -19,5 +19,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_12_205346) do
     t.boolean "deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "thickness"
+    t.string "standard_measures"
+    t.integer "quantity"
+    t.string "location"
+    t.string "status"
+    t.boolean "is_scrap"
   end
 end
