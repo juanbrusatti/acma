0276ff2e5f3c54d1a2398f15091763885900c5dd
Mateo Cornejo <ccornejomateo@gmail.com>
2025-07-16 19:20:42 -0300

    the dvh model was created

--- Aberturas/app/models/glasscutting.rb
+++ Aberturas/app/models/glasscutting.rb
@@ -1,6 +1,5 @@
 class Glasscutting < ApplicationRecord
   belongs_to :project
-  belongs_to :dvh, optional: true
   belongs_to :glassplate, optional: true
 
   validates :height, :width, presence: true, numericality: { greater_than: 0 }
