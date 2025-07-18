60590e0caa00f6a982ade61bd6871296a1e376e3
Juan Brusatti <juanbrusatti@gmail.com>
2025-07-18 19:08:22 -0300

    mofified: db/schema.rb

--- Aberturas/db/schema.rb
+++ Aberturas/db/schema.rb
@@ -10,7 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_13_233741) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_18_205037) do
+  create_table "dvhs", force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "innertube"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "height"
+    t.float "width"
+    t.string "location"
+    t.decimal "price"
+    t.string "glasscutting1_type"
+    t.string "glasscutting1_thickness"
+    t.string "glasscutting1_color"
+    t.string "glasscutting2_type"
+    t.string "glasscutting2_thickness"
+    t.string "glasscutting2_color"
+    t.index ["project_id"], name: "index_dvhs_on_project_id"
+  end
+
+  create_table "glasscuttings", force: :cascade do |t|
+    t.float "height"
+    t.float "width"
+    t.string "color"
+    t.string "glass_type"
+    t.string "location"
+    t.decimal "price"
+    t.integer "project_id", null: false
+    t.integer "dvh_id"
+    t.integer "glassplate_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "thickness"
+    t.index ["dvh_id"], name: "index_glasscuttings_on_dvh_id"
+    t.index ["glassplate_id"], name: "index_glasscuttings_on_glassplate_id"
+    t.index ["project_id"], name: "index_glasscuttings_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,5 +54,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_13_233741) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.date "delivery_date"
+    t.string "phone"
+    t.string "address"
   end
 end
