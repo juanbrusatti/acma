5c1246f3fb45083f822b3e037ed4ef7c501e9bf5
Mateo Cornejo <ccornejomateo@gmail.com>
2025-07-29 15:54:50 -0300

    modify the price table to include the columns ‘purchase price’, ‘profit percentage’, and ‘selling price’

--- Aberturas/db/schema.rb
+++ Aberturas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_24_000637) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_29_172344) do
   create_table "dvhs", force: :cascade do |t|
     t.integer "project_id", null: false
     t.integer "innertube"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_000637) do
     t.string "glasscutting2_color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "typology"
     t.index ["project_id"], name: "index_dvhs_on_project_id"
   end
 
@@ -33,10 +34,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_000637) do
     t.string "color"
     t.string "glass_type"
     t.string "thickness"
-    t.decimal "price"
-    t.decimal "price_m2"
+    t.decimal "buying_price"
+    t.decimal "selling_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "percentage", precision: 5, scale: 2
   end
 
   create_table "glasscuttings", force: :cascade do |t|
@@ -52,6 +54,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_000637) do
     t.integer "glassplate_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sequential_id"
+    t.string "typology"
     t.index ["dvh_id"], name: "index_glasscuttings_on_dvh_id"
     t.index ["glassplate_id"], name: "index_glasscuttings_on_glassplate_id"
     t.index ["project_id"], name: "index_glasscuttings_on_project_id"
@@ -83,6 +87,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_000637) do
     t.string "address"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.decimal "price"
+    t.decimal "priceWithoutIva"
+    t.decimal "price_without_iva"
   end
 
   create_table "supplies", force: :cascade do |t|
