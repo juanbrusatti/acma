3e2293072bc1360aeba0e19dfe0927afaebce50c
Mateo Cornejo <ccornejomateo@gmail.com>
2025-08-16 13:17:14 -0300

    i display the tables as shown in the photo, and I use the same form for new and edit

--- docker/Aberturas/app/models/scrap.rb
+++ docker/Aberturas/app/models/scrap.rb
@@ -5,36 +5,25 @@ class Scrap < ApplicationRecord
 
   # Color validation
   validates :color, presence: true
-  validates :color, inclusion: {
-    in: ["INC", "STB", "GRS", "BRC", "BLS", "STG", "NTR"],
-    message: "debe ser uno de: INC, STB, GRS, BRC, BLS, STG, NTR"
-  }
+  validates :color, inclusion: { in: ["INC", "STB", "GRS", "BRC", "BLS", "STG", "NTR"] }
 
   # Glass type validation
   validates :scrap_type, presence: true
-  validates :scrap_type, inclusion: {
-    in: ["LAM", "FLO", "COL"],
-    message: "debe ser uno de: LAM, FLO, COL"
-  }
+  validates :scrap_type, inclusion: { in: ["LAM", "FLO", "COL"] }
 
   # Validations for width and height
-  validates :width, :height, presence: true, numericality: { greater_than: 0 }
-
+  validates :height, presence: true, numericality: { greater_than: 0 }
+  validates :width, presence: true, numericality: { greater_than: 0 }
+  
   # Thickness validation
   validates :thickness, presence: true
-  validates :thickness, inclusion: {
-    in: [ "3+3", "4+4", "5+5", "5mm" ],
-    message: "debe ser uno de: 3+3, 4+4, 5+5, 5mm"
-  }
+  validates :thickness, inclusion: { in: [ "3+3", "4+4", "5+5", "5mm" ] }
 
   # Output work validation
   validates :output_work, presence: true
 
   # Status validation
   validates :status, presence: true
-  validates :status, inclusion: {
-    in: ["Disponible", "Reservado"],
-    message: "debe ser uno de: Disponible, Reservado"
-  }
+  validates :status, inclusion: { in: ["Disponible", "Reservado"] }
 
 end
