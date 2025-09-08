c72e1b6a9a351907c089ec1aa2efe63d9d665bce
Juan Brusatti <juanbrusatti@gmail.com>
2025-09-08 19:00:36 -0300

    add select option in new glassplate

--- docker/Aberturas/app/javascript/application.js
+++ docker/Aberturas/app/javascript/application.js
@@ -6,9 +6,15 @@ import "projects_inline_edit"
 import "glass_prices_calculator"
 import "supply_editing"
 import "preview_pdf"
+import "glassplate_selects"
+import "scrap_selects"
 
 
 document.addEventListener("turbo:load", () => {
+	// Inicializar selects automáticamente
+	setupAllGlassplateSelects();
+	setupAllScrapSelects();
+	
 	// Exponer funciones para depuración manual
 	window.setupAllScrapSelects = setupAllScrapSelects;
 	window.updateScrapSelects = updateScrapSelects;
