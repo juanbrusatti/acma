3186d988e7e8bc3678dd99db225a2995c9614d07
Baachi13 <jeremiasavaro7@gmail.com>
2025-09-11 20:00:39 -0300

    make the buttons for optimizer and stock clickables

--- docker/Aberturas/config/routes.rb
+++ docker/Aberturas/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :scraps
   resources :supplies
-
-  # resources :glassplates
+  resources :glassplates
 
   get "static_pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
