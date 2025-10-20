c04a05afc36aea0d1ea5165660def53b5c152a89
Baachi13 <jeremiasavaro7@gmail.com>
2025-10-20 19:52:37 -0300

    Add date_of_optimization attribute to projects and update schema

--- docker/Aberturas/db/schema.rb
+++ docker/Aberturas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_210512) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_20_224540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -110,6 +110,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_17_210512) do
     t.datetime "updated_at", precision: nil, null: false
     t.decimal "price"
     t.decimal "price_without_iva"
+    t.date "date_of_optimization"
   end
 
   create_table "scraps", force: :cascade do |t|
