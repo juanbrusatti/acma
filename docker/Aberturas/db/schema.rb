6af3030c0e9d1054301b30a8f76bfdec1736ea22
Mateo Cornejo <ccornejomateo@gmail.com>
2026-09-15 22:54:58 -0300

    Add location to scraps table

--- docker/Aberturas/db/schema.rb
+++ docker/Aberturas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_18_010820) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_15_171109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -123,6 +123,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_18_010820) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.string "input_work"
+    t.string "location"
   end
 
   create_table "supplies", force: :cascade do |t|
